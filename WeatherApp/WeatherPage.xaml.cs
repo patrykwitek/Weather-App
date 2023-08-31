97429fdaca76b0037fb38954e17936bd252c8516
Patryk Witek <patryk.witolski@gmail.com>
2023-08-31 19:20:05 +0200

    get weather by current location & location button

--- WeatherApp/WeatherPage.xaml.cs
+++ WeatherApp/WeatherPage.xaml.cs
@@ -5,6 +5,8 @@ namespace WeatherApp;
 public partial class WeatherPage : ContentPage
 {
 	public List<Models.List> WeatherList;
+	private double latitude;
+	private double longitude;
 	public WeatherPage()
 	{
 		InitializeComponent();
@@ -14,18 +16,37 @@ public partial class WeatherPage : ContentPage
 	protected async override void OnAppearing()
 	{
 		base.OnAppearing();
-		var result = await ApiService.GetWeather(50.054810, 19.927839);
-		foreach(var item in result.list)
-		{
-			WeatherList.Add(item);
-		}
-		CvWeather.ItemsSource = WeatherList;
+		await GetLocation();
+        await GetWeatherDataByLocation(latitude, longitude);
+    }
 
-		LblCity.Text = result.city.name;
-		LblWeatherDescription.Text = result.list[0].weather[0].description;
-		LblTemperature.Text = result.list[0].main.temperature + "°C";
-		LblHumidity.Text = result.list[0].main.humidity + "%";
-		LblWind.Text = result.list[0].wind.speed + "km/h";
-		ImgWeatherIcon.Source = result.list[0].weather[0].customIcon + ".png";
+	public async Task GetLocation()
+	{
+		Location location = await Geolocation.GetLocationAsync();
+		latitude = location.Latitude;
+		longitude = location.Longitude;
 	}
+
+	private async void TapLocation_Tapped(object sender, EventArgs e)
+	{
+		await GetLocation();
+		await GetWeatherDataByLocation(latitude, longitude);
+	}
+
+	public async Task GetWeatherDataByLocation(double latitude, double longitude)
+	{
+        var result = await ApiService.GetWeather(latitude, longitude);
+        foreach (var item in result.list)
+        {
+            WeatherList.Add(item);
+        }
+        CvWeather.ItemsSource = WeatherList;
+
+        LblCity.Text = result.city.name;
+        LblWeatherDescription.Text = result.list[0].weather[0].description;
+        LblTemperature.Text = result.list[0].main.temperature + "°C";
+        LblHumidity.Text = result.list[0].main.humidity + "%";
+        LblWind.Text = result.list[0].wind.speed + "km/h";
+        ImgWeatherIcon.Source = result.list[0].weather[0].customIcon + ".png";
+    }
 }
